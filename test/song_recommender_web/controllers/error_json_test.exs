806839cbec2f5c9a8f2f43f2c8d98501d3245f75
Dean Kinyua <kinyuadean@gmail.com>
2026-04-27 14:40:31 +0300

    Configure app theme (#2)
    * add custom buttons, colors, remove dark theme and add the montserrat font

--- test/song_recommender_web/controllers/error_json_test.exs
+++ test/song_recommender_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule SongRecommenderWeb.ErrorJSONTest do
   use SongRecommenderWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert SongRecommenderWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert SongRecommenderWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
