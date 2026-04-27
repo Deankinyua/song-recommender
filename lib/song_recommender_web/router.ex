c4dc7a87eb84551e41bfe4d73a288487eb2ac1ef
Dean Kinyua <kinyuadean@gmail.com>
2026-04-27 15:26:59 +0300

    add routes for sign in, sign out, genres e.t.c

--- lib/song_recommender_web/router.ex
+++ lib/song_recommender_web/router.ex
@@ -1,6 +1,8 @@
 defmodule SongRecommenderWeb.Router do
   use SongRecommenderWeb, :router
 
+  import SongRecommenderWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,22 +10,44 @@ defmodule SongRecommenderWeb.Router do
     plug :put_root_layout, html: {SongRecommenderWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", SongRecommenderWeb do
-    pipe_through :browser
-
-    get "/", PageController, :home
-  end
-
   # Other scopes may use custom stacks.
   # scope "/api", SongRecommenderWeb do
   #   pipe_through :api
   # end
+
+  scope "/", SongRecommenderWeb do
+    pipe_through [:browser]
+
+    delete "/sign-out", UserSessionController, :log_out
+    post "/sign-in", UserSessionController, :create
+  end
+
+  scope "/", SongRecommenderWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{SongRecommenderWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/sign-up", AuthLive.SignUp, :new
+      live "/sign-in", AuthLive.SignIn, :new
+    end
+  end
+
+  scope "/", SongRecommenderWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{SongRecommenderWeb.UserAuth, :ensure_authenticated}] do
+      live "/", SongsLive.Index, :index
+      live "/genres", GenresLive.Index, :index
+    end
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:song_recommender, :dev_routes) do
