f4add7f649276efc4d426e0fe4fce8c5083afb2b
Dean Kinyua <kinyuadean@gmail.com>
2026-04-27 14:11:01 +0300

    use JS conventions

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,8 +22,12 @@ import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
-import { hooks as colocatedHooks } from "phoenix-colocated/song_recommender";
+import { hooks as ColocatedHooks } from "phoenix-colocated/song_recommender";
 import topbar from "../vendor/topbar";
+
+let Hooks = {
+  ...ColocatedHooks,
+};
 
 const csrfToken = document
   .querySelector("meta[name='csrf-token']")
@@ -31,7 +35,7 @@ const csrfToken = document
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: { _csrf_token: csrfToken },
-  hooks: { ...colocatedHooks },
+  hooks: Hooks,
 });
 
 // Show progress bar on live navigation and form submits
