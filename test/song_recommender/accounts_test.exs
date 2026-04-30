6f6c4f49febefbf62327dd68fcdf51aa17d8ba13
Dean Kinyua <kinyuadean@gmail.com>
2026-04-30 16:15:54 +0300

    accounts test

--- test/song_recommender/accounts_test.exs
+++ test/song_recommender/accounts_test.exs
@@ -1,33 +1,44 @@
 defmodule SongRecommender.AccountsTest do
   use SongRecommender.DataCase, async: true
 
-  # import SongRecommender.AccountsFixtures
+  import SongRecommender.AccountsFixtures
 
   alias SongRecommender.Accounts
   alias SongRecommender.Accounts.User
 
-  @valid_attrs %{
-    "name" => "Marion",
-    "yob" => 1996
-  }
+  @invalid_user_attrs %{name: "Marion", yob: nil}
+  @valid_user_attrs %{name: "Marion", yob: 1996}
 
-  # defp create_user_and_token(_attrs) do
-  #   user = user_fixture()
-  #   token = Accounts.generate_user_session_token(user)
-  #   %{user: user, token: token}
-  # end
+  defp create_user(_attrs) do
+    user = user_fixture()
+    %{user: user}
+  end
 
   describe "register_user/1" do
-    test "with valid data creates a user" do
-      answer = Accounts.register_user(@valid_attrs)
+    test "with valid data, creates a user" do
+      {:ok, %User{} = user} = Accounts.register_user(@valid_user_attrs)
 
-      dbg(answer)
-
-      :ok
+      assert user.name == "Marion"
+      assert user.yob == 1996
     end
 
-    # test "with invalid data returns error changeset" do
-    #   assert {:error, %Ecto.Changeset{}} = Accounts.register_user(@invalid_attrs)
-    # end
+    test "with invalid data, returns error changeset" do
+      assert {:error, %Ecto.Changeset{} = changeset} = Accounts.register_user(@invalid_user_attrs)
+
+      assert "can't be blank" in errors_on(changeset).yob
+    end
+  end
+
+  describe "get_user!/1" do
+    setup [:create_user]
+
+    test "returns a user if they exist", %{user: user} do
+      retrieved_user = Accounts.get_user!(user.name)
+      assert retrieved_user.yob == user.yob
+    end
+
+    test "returns nil for a user that doesn't exist" do
+      refute Accounts.get_user!("nonexistentuser")
+    end
   end
 end
