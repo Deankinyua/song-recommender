79057070653b6aeb3f4535a7c5f657e6f05ac075
Dean Kinyua <kinyuadean@gmail.com>
2026-04-30 17:55:37 +0300

    add more tests

--- test/song_recommender/accounts_test.exs
+++ test/song_recommender/accounts_test.exs
@@ -9,9 +9,10 @@ defmodule SongRecommender.AccountsTest do
   @invalid_user_attrs %{name: "Marion", yob: nil}
   @valid_user_attrs %{name: "Marion", yob: 1996}
 
-  defp create_user(_attrs) do
+  defp create_user_and_token(_attrs) do
     user = user_fixture()
-    %{user: user}
+    token = Accounts.generate_user_session_token(user.name)
+    %{token: token, user: user}
   end
 
   describe "register_user/1" do
@@ -30,7 +31,7 @@ defmodule SongRecommender.AccountsTest do
   end
 
   describe "get_user!/1" do
-    setup [:create_user]
+    setup [:create_user_and_token]
 
     test "returns a user if they exist", %{user: user} do
       retrieved_user = Accounts.get_user!(user.name)
@@ -39,6 +40,40 @@ defmodule SongRecommender.AccountsTest do
 
     test "returns nil for a user that doesn't exist" do
       refute Accounts.get_user!("nonexistentuser")
+    end
+  end
+
+  describe "generate_user_session_token/1" do
+    setup [:create_user_and_token]
+
+    test "adds an auth token for a user", %{user: user} do
+      token = Accounts.generate_user_session_token(user.name)
+      retrieved_user = Accounts.get_user_by_session_token(token)
+      assert retrieved_user.yob == user.yob
+      assert retrieved_user.name == user.name
+    end
+  end
+
+  describe "get_user_by_session_token/1" do
+    setup [:create_user_and_token]
+
+    test "returns a user if the token is valid", %{token: token, user: user} do
+      retrieved_user = Accounts.get_user_by_session_token(token)
+      assert retrieved_user.yob == user.yob
+      assert retrieved_user.name == user.name
+    end
+
+    test "returns nil when the token is invalid" do
+      refute Accounts.get_user_by_session_token("2ZWlPOoWh0626oTaHrnl2a")
+    end
+  end
+
+  describe "delete_user_session_token/1" do
+    setup [:create_user_and_token]
+
+    test "deletes a user session token", %{token: token} do
+      Accounts.delete_user_session_token(token)
+      refute Accounts.get_user_by_session_token(token)
     end
   end
 end
