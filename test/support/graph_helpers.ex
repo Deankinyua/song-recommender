9fc97b11a656b9549f22b6707547f334ddb4b8e3
Dean Kinyua <kinyuadean@gmail.com>
2026-05-04 16:29:51 +0300

    test the create listening history functionality

--- test/support/graph_helpers.ex
+++ test/support/graph_helpers.ex
@@ -4,6 +4,7 @@ defmodule SongRecommender.GraphHelpers do
   """
 
   @type bolt_response :: Boltx.Response.t()
+  @type genre :: String.t()
 
   @doc """
   Clears the graph so that the next test can start on a clean slate
@@ -19,4 +20,46 @@ defmodule SongRecommender.GraphHelpers do
       """
     )
   end
+
+  @spec create_songs_with_genre(genre()) :: bolt_response()
+  def create_songs_with_genre(genre) do
+    Bolt
+    |> Boltx.query!(
+      """
+       CREATE (song_1:Song {id: "6VtoP2sJt5oCmPOQIve2sf", duration_ms: 166240, name: "Someone Like You", popularity: 70, released: 2012}),
+              (song_2:Song {id: "2EKxmYmUdAVXlaHCnnW13o", duration_ms: 220160, name: "Heaven", popularity: 58, released: 2012}),
+              (song_3:Song {id: "7KG9zriC6iP8F1CNihtR8Y", duration_ms: 216387, name: "Princess", popularity: 39, released: 2014}),
+
+              (genre_1:Genre {name: $genre}),
+              (artist_1:Artist {name: 'Kendrick'}),
+              (artist_2:Artist {name: 'Drake'}),
+
+              (song_1)-[:BELONGS_TO]->(genre_1),
+              (song_2)-[:BELONGS_TO]->(genre_1),
+              (song_3)-[:BELONGS_TO]->(genre_1),
+              (artist_1)-[:SANG]->(song_1),
+              (artist_1)-[:SANG]->(song_2),
+              (artist_2)-[:SANG]->(song_3)
+       RETURN [song_1.id, song_2.id, song_3.id] AS songs
+      """,
+      %{genre: genre}
+    )
+    |> Boltx.Response.first()
+    |> return_song_ids()
+  end
+
+  def check_for_listens(username, song_id) do
+    Bolt
+    |> Boltx.query!(
+      """
+      RETURN exists( (:User {name: $name})-[:LISTENED_TO]->(:Song {id: $song_id}) ) AS listened_status
+      """,
+      %{name: username, song_id: song_id}
+    )
+    |> Boltx.Response.first()
+    |> user_listened_to_song?()
+  end
+
+  defp return_song_ids(%{"songs" => song_ids}), do: song_ids
+  defp user_listened_to_song?(%{"listened_status" => status}), do: status
 end
