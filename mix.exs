3fad35cc5de7774ad908300bc9e6d7b7600d2b41
Dean Kinyua <kinyuadean@gmail.com>
2026-05-08 10:31:19 +0300

    upgrade Bandit since the current version is vulnerable

--- mix.exs
+++ mix.exs
@@ -98,7 +98,7 @@ defmodule SongRecommender.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.11"}
     ]
   end
 
