04334d3c590ed43b89da95d88cda6c640121b012
Dean Kinyua <kinyuadean@gmail.com>
2026-05-12 14:48:07 +0300

    add the 4 icons: back, next, skip and pause icons

--- lib/song_recommender_web/components/custom_components.ex
+++ lib/song_recommender_web/components/custom_components.ex
@@ -60,9 +60,9 @@ defmodule SongRecommenderWeb.CustomComponents do
   def search(assigns) do
     ~H"""
     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-      <g id="SVGRepo_iconCarrier">
+      <g stroke-width="0"></g>
+      <g stroke-linecap="round" stroke-linejoin="round"></g>
+      <g>
         <g clip-path="url(#clip0_15_152)">
           <rect width="24" height="24" fill="currentBackgroundColor"></rect>
 
@@ -129,6 +129,110 @@ defmodule SongRecommenderWeb.CustomComponents do
     <div class="rounded-md py-2 px-2 mx-3 happy-monkey-regular">
       Recent searches
     </div>
+    """
+  end
+
+  @spec back_icon(assigns()) :: rendered()
+  def back_icon(assigns) do
+    ~H"""
+    <svg
+      fill="#000000"
+      width="20px"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 512.005 512.005"
+      xml:space="preserve"
+    >
+      <g id="SVGRepo_bgCarrier_1" stroke-width="0"></g>
+      <g id="SVGRepo_tracerCarrier_1" stroke-linecap="round" stroke-linejoin="round"></g>
+      <g id="SVGRepo_iconCarrier_1">
+        <g>
+          <g>
+            <path d="M490.861,2.971c-6.485-3.84-14.507-3.968-21.141-0.32L53.336,231.664V21.338c0-11.776-9.557-21.333-21.333-21.333 S10.669,9.562,10.669,21.338v469.333c0,11.776,9.557,21.333,21.333,21.333s21.333-9.557,21.333-21.333V280.346L469.72,509.36 c3.221,1.771,6.741,2.645,10.283,2.645c3.755,0,7.531-1.003,10.859-2.965c6.507-3.84,10.475-10.837,10.475-18.368V21.338 C501.336,13.786,497.368,6.81,490.861,2.971z">
+            </path>
+          </g>
+        </g>
+      </g>
+    </svg>
+    """
+  end
+
+  @spec next_icon(assigns()) :: rendered()
+  def next_icon(assigns) do
+    ~H"""
+    <svg
+      fill="#000000"
+      width="20px"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 512 512"
+      xml:space="preserve"
+    >
+      <g id="SVGRepo_bgCarrier_2" stroke-width="0"></g>
+      <g id="SVGRepo_tracerCarrier_2" stroke-linecap="round" stroke-linejoin="round"></g>
+      <g id="SVGRepo_iconCarrier_2">
+        <g>
+          <g>
+            <path d="M480,0c-11.776,0-21.333,9.557-21.333,21.333v210.325L42.283,2.645c-6.613-3.627-14.656-3.52-21.141,0.32 c-6.485,3.84-10.475,10.816-10.475,18.368v469.333c0,7.552,3.989,14.528,10.475,18.368C24.491,511.019,28.245,512,32,512 c3.541,0,7.083-0.875,10.283-2.645l416.384-229.013v210.325c0,11.776,9.557,21.333,21.333,21.333s21.333-9.557,21.333-21.333 V21.333C501.333,9.557,491.776,0,480,0z">
+            </path>
+          </g>
+        </g>
+      </g>
+    </svg>
+    """
+  end
+
+  @spec skip_icon(assigns()) :: rendered()
+  def skip_icon(assigns) do
+    ~H"""
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier_3" stroke-width="0"></g>
+      <g id="SVGRepo_tracerCarrier_3" stroke-linecap="round" stroke-linejoin="round"></g>
+      <g id="SVGRepo_iconCarrier_3">
+        <path d="M2 18.3429L10.7429 12.1714L2 6V18.3429Z" fill="#000000"></path>
+
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M20 12.1714L11.2571 18.3429V6L20 12.1714ZM20 12.1714V6H22V18H20V12.1714Z"
+          fill="#000000"
+        >
+        </path>
+      </g>
+    </svg>
+    """
+  end
+
+  @spec play_pause_icon(assigns()) :: rendered()
+  def play_pause_icon(assigns) do
+    ~H"""
+    <svg id="pause-play" width="36" viewBox="0 0 36 36" phx-hook="PausePlay" fill="white">
+      <circle cx="18" cy="18" r="17" class="play-btn-circle" />
+
+      <polygon
+        id="polygon-1"
+        points="
+            11,10
+            15,10
+            15,26
+            11,26
+          "
+        fill="currentColor"
+      />
+
+      <polygon
+        id="polygon-2"
+        points="
+            21,10
+            25,10
+            25,26
+            21,26
+          "
+        fill="currentColor"
+      />
+    </svg>
     """
   end
 
