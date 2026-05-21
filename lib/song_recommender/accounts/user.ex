21d893b8696b2c161bf687e9ddcb869b6e6ee389
Dean Kinyua <kinyuadean@gmail.com>
2026-05-21 10:44:11 +0300

    change the user schema to exclude genres

--- lib/song_recommender/accounts/user.ex
+++ lib/song_recommender/accounts/user.ex
@@ -7,25 +7,20 @@ defmodule SongRecommender.Accounts.User do
 
   import Ecto.Changeset
 
-  @valid_genres Application.compile_env!(:song_recommender, :genres)
-
   @type attrs :: map()
   @type changeset :: Ecto.Changeset.t()
   @type t :: %__MODULE__{}
 
   @primary_key false
   embedded_schema do
-    field :genres, {:array, :string}, default: []
     field :name, :string
   end
 
-  @spec changeset(t(), attrs()) ::
-          changeset()
+  @spec changeset(t(), attrs()) :: changeset()
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:genres, :name])
+    |> cast(attrs, [:name])
     |> validate_required([:name])
     |> validate_length(:name, min: 4, message: "Your name must be at least 4 characters long")
-    |> validate_subset(:genres, @valid_genres, message: "You selected an invalid entry")
   end
 end
