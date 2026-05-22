a64c65d1934a0a7cbbab3d0d67f0d1cc61be825d
Dean Kinyua <kinyuadean@gmail.com>
2026-05-22 23:04:27 +0300

    avoid fetching songs when the taste_profile is nil for both artists and genres

--- lib/song_recommender/query_engine.ex
+++ lib/song_recommender/query_engine.ex
@@ -10,18 +10,19 @@ defmodule SongRecommender.QueryEngine do
         :genre_based,
         %{genres: genres_profile, artists: artists_profile} = _profile
       ) do
-    %{nodes: genres, limit: song_limit} = genres_profile
-
-    songs_from_genres = Genres.get_songs_from_genre(genres, song_limit)
-    songs_from_artists = get_songs_from_artists(artists_profile)
+    songs_from_genres = get_songs(:genre, genres_profile)
+    songs_from_artists = get_songs(:artist, artists_profile)
 
     songs_from_genres ++ songs_from_artists
   end
 
   def get_initial_songs(:hybrid, _profile), do: :ok
 
-  defp get_songs_from_artists(artists_profile) when is_nil(artists_profile), do: []
+  defp get_songs(_node_type, profile) when is_nil(profile), do: []
 
-  defp get_songs_from_artists(%{nodes: artists, limit: song_limit} = _artists_profile),
+  defp get_songs(:artist, %{nodes: artists, limit: song_limit} = _artists_profile),
     do: Artists.get_songs_from_artists(artists, song_limit)
+
+  defp get_songs(:genre, %{nodes: genres, limit: song_limit} = _genres_profile),
+    do: Genres.get_songs_from_genre(genres, song_limit)
 end
