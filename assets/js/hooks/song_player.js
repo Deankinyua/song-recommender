e5fad4f51c0f48098cfa6498cd5dc66c35332b47
Dean Kinyua <kinyuadean@gmail.com>
2026-05-26 03:53:21 +0300

    refactor playPause animation to a separate async function that will return the isStopped variable value once it is done

--- assets/js/hooks/song_player.js
+++ assets/js/hooks/song_player.js
@@ -1,9 +1,4 @@
-import {
-  getProgress,
-  easeInOut,
-  buildShapeTransition,
-  returnPolygonPoints,
-} from "js/helpers/animation_helpers.js";
+import { animatePausePlayButton } from "../helpers/play_pause_animation.js";
 
 let SongPlayerHooks = {};
 
@@ -18,11 +13,6 @@ SongPlayerHooks.SongPlayer = {
     const songProgress = document.getElementById("song-progress");
     const songDurationEl = document.getElementById("song-duration");
     const playPauseTooltipEl = document.getElementById("pause-play-tooltip");
-
-    const time = {
-      start: null,
-      total: 500,
-    };
 
     let isStopped = true;
 
@@ -78,47 +68,11 @@ SongPlayerHooks.SongPlayer = {
       player.currentTime = Number(e.target.value);
     });
 
-    playBtn.addEventListener("click", () => {
-      requestAnimationFrame(playOrStop);
+    playBtn.addEventListener("click", async () => {
+      isStopped = await animatePausePlayButton(isStopped, polygon_1, polygon_2);
       player.isPlaying = !player.isPlaying;
       playPauseTooltipEl.textContent = player.isPlaying ? "Pause" : "Play";
     });
-
-    const playOrStop = (now) => {
-      if (!time.start) time.start = now;
-      time.elapsed = now - time.start;
-
-      const progress = getProgress(time);
-      const easing = easeInOut(progress);
-
-      let {
-        start_shape_polygon_1,
-        end_shape_polygon_1,
-        start_shape_polygon_2,
-        end_shape_polygon_2,
-      } = buildShapeTransition(isStopped);
-
-      const polygon_1_points = returnPolygonPoints(
-        start_shape_polygon_1,
-        end_shape_polygon_1,
-        easing,
-      );
-
-      const polygon_2_points = returnPolygonPoints(
-        start_shape_polygon_2,
-        end_shape_polygon_2,
-        easing,
-      );
-
-      polygon_1.setAttribute("points", polygon_1_points.join(" "));
-      polygon_2.setAttribute("points", polygon_2_points.join(" "));
-
-      if (progress < 1) requestAnimationFrame(playOrStop);
-      if (progress >= 1) {
-        isStopped = !isStopped;
-        time.start = null;
-      }
-    };
   },
 };
 
